038b83efa7c178460abc84f1e9948e5c36298f9b
Elena <Elena-Bonito@yandex.ru>
2019-06-07 00:49:33 +0500

    правки: переполнение

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var hotelSearch = document.querySelector(".button-search");
+var form = document.querySelector(".reservation-form");
+hotelSearch.addEventListener("click", function(){
+  form.classList.toggle("hide");
+});

a5214d22fd42b7554deef1c097b85499f733ed34
Elena <Elena-Bonito@yandex.ru>
2019-06-07 18:38:54 +0500

    добавление Js, отладка карты

--- js/script.js
+++ js/script.js
@@ -1,5 +1,37 @@
 var hotelSearch = document.querySelector(".button-search");
 var form = document.querySelector(".reservation-form");
-hotelSearch.addEventListener("click", function(){
-  form.classList.toggle("hide");
+var arrival = form.querySelector("[name=arrival-date]");
+var departure = form.querySelector("[name=departure-date]");
+
+hotelSearch.addEventListener("click", function(evt){
+  evt.preventDefault();
+  form.classList.toggle("show");
+});
+
+form.addEventListener("submit", function(evt){
+  if(!arrival.value && departure.value){
+    evt.preventDefault();
+    arrival.classList.add("input-error-folder");
+    form.classList.add("input-error");
+  }
+
+  else if(arrival.value && !departure.value){
+    evt.preventDefault();
+    departure.classList.add("input-error-folder");
+    form.classList.add("input-error");
+  }
+
+  else{evt.preventDefault();
+    departure.classList.add("input-error-folder");
+    arrival.classList.add("input-error-folder");
+    form.classList.add("input-error");}
+})
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (form.classList.contains("show")) {
+      form.classList.remove("show");
+    }
+  }
 });

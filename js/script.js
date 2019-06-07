f19cd5559d047f46e93279a94157a5bd713f681d
Elena <Elena-Bonito@yandex.ru>
2019-06-08 01:52:43 +0500

    добавление localStorage

--- js/script.js
+++ js/script.js
@@ -2,6 +2,26 @@ var hotelSearch = document.querySelector(".button-search");
 var form = document.querySelector(".reservation-form");
 var arrival = form.querySelector("[name=arrival-date]");
 var departure = form.querySelector("[name=departure-date]");
+var adultsNumber = form.querySelector("[name=adults]");
+var kidsNumber = form.querySelector("[name=kids]");
+
+var isStorageSupportAdults = true;
+var storageAdults = "";
+
+try {
+    storageAdults = localStorage.getItem("adultsNumber");
+} catch (err) {
+    isStorageSupportAdults = false;
+}
+
+var isStorageSupportKids = true;
+var storageKids = "";
+
+try {
+    storageKids = localStorage.getItem("kidsNumber");
+} catch (err) {
+    isStorageSupportKids = false;
+}
 
 hotelSearch.addEventListener("click", function(evt){
   evt.preventDefault();
@@ -21,10 +41,21 @@ form.addEventListener("submit", function(evt){
     form.classList.add("input-error");
   }
 
-  else{evt.preventDefault();
+  else{
+    evt.preventDefault();
     departure.classList.add("input-error-folder");
     arrival.classList.add("input-error-folder");
     form.classList.add("input-error");}
+
+})
+
+form.addEventListener("submit", function(){
+  if(arrival.value && departure.value && adultsNumber.value && kidsNumber.value){
+    if (isStorageSupportAdults && isStorageSupportKids) {
+      localStorage.setItem("adultsNumber", adultsNumber.value);
+      localStorage.setItem("kidsNumber", kidsNumber.value);
+    }
+  }
 })
 
 window.addEventListener("keydown", function (evt) {
